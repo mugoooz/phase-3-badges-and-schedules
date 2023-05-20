94a59b96682b0d601553a3a62531d37077a85a20
Eric Mugo <mugoericpg20@gmail.com>
2023-05-20 06:57:01 +0300

    Added badge maker and creator methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}"
+
+end
+
+def batch_badge_creator(name)
+    badge = []
+    name.each do |name|
+        badge << "Hello, my name is #{name}"
+    end
+    badge
+end
